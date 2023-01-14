eecb4b4441b8811528f227dee61fbb0a3fc2b8ed
Ryan <havepepsicola@gmail.com>
2023-01-13 18:28:19 -0800

    Updated file located.
    Moved files out of folder.

---
+++ src/Workspace/SignsInternal/ReadMe.server.lua
@@ -0,0 +1,5 @@
+--[[
+	Please do not steal this plugin.
+	You WILL get a copyright notice and this WILL be the grounds for your copied plugin's takedown.
+	No one has permitted access to read or reproduce this plugin.
+--]]
