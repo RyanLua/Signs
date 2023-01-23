098de7c2378b363942fc7908f3b29448034288f1
Ryan <havepepsicola@gmail.com>
2023-01-23 10:59:04 -0800

    Added alignment support.

---
+++ src/Workspace/SignsInternal/PluginGui/HorizontalAlignment.lua
@@ -0,0 +1,18 @@
+local choices = {
+
+	{Id = "1",
+		Mode = Enum.HorizontalAlignment.Left,
+		Text = "Left",
+	},
+	{Id = "2",
+		Mode = Enum.HorizontalAlignment.Center,
+		Text = "Center",
+	},
+	{Id = "3",
+		Mode = Enum.HorizontalAlignment.Right,
+		Text = "Right",
+	},
+
+}
+
+return choices
