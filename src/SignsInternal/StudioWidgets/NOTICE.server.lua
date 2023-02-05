85ac99e86fe36bd04eb9d9a0a086ce192ce80402
Ryan <havepepsicola@gmail.com>
2023-02-04 17:03:26 -0800

    Updated to specify changes.

--- src/SignsInternal/StudioWidgets/NOTICE.server.lua
+++ src/SignsInternal/StudioWidgets/NOTICE.server.lua
@@ -1,23 +1,27 @@
 --[[
-   Studio Widgets is a set of GUI elements to use in Roblox Plugins hosted in PluginGUIs. Widgets have a standard "Studio" look & feel.
-   Copyright 2020 Roblox
+Studio Widgets is a set of GUI elements to use in Roblox Plugins hosted in PluginGUIs. Widgets have a standard "Studio" look & feel.
+Copyright 2020 Roblox
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
-   Changes to Studio Widgets include minor and major modifications to it's module scripts which include:
-    - Bug fixes
-    - New modules
-    - Rewritten modules
-    - Removal of unused code
-    - and other minor changes
+Changes to Studio Widgets include minor and major modifications to it's files which include:
+ a. New files
+	- New file additions.
+ b. Substituted files
+	- Replacements to file to fix bugs or unexpected behavior.
+ c. Rewritten files
+	- Modifications to files to improve readability.
+ d. Removal of unused files
+	- Removal of unused files such as a extra unimplemented library.
+ e. and other minor changes
 ]]
