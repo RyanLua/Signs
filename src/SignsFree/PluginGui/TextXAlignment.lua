04dd45c83ce2f3815c37f9dc9e33b368a059ad63
RyanLua <80087248+RyanLua@users.noreply.github.com>
2023-04-27 12:56:08 -0700

    Update Signs Free.
    Signed-off-by: RyanLua <80087248+RyanLua@users.noreply.github.com>

---
+++ src/SignsFree/PluginGui/TextXAlignment.lua
@@ -0,0 +1,7 @@
+local choices = {
+	{ Id = "1", Mode = Enum.TextXAlignment.Left, Text = "Left" },
+	{ Id = "2", Mode = Enum.TextXAlignment.Center, Text = "Center" },
+	{ Id = "3", Mode = Enum.TextXAlignment.Right, Text = "Right" },
+}
+
+return choices
