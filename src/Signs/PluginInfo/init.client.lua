d161b2f740a2a0a9b8e02527b87e286b31b097df
RyanLua <80087248+RyanLua@users.noreply.github.com>
2023-04-27 11:43:10 -0700

    Update version
    Signed-off-by: RyanLua <80087248+RyanLua@users.noreply.github.com>

--- src/Signs/PluginInfo/init.client.lua
+++ src/Signs/PluginInfo/init.client.lua
@@ -27,7 +27,7 @@ button.ClickableWhenViewportHidden = true
 
 -- Create new widget GUI and name it
 local widget = plugin:CreateDockWidgetPluginGui("Signs", widgetInfo)
-widget.Title = "Signs 3.0.0"
+widget.Title = "Signs 3.1.0"
 widget.Name = "Signs"
 
 -- Initialize PluginGui or DevelopmentGui
